f61a6cad353ba8bf21a453f02d82b58d89ef97d6
Matt Carroll <mcarroll15@huskers.unl.edu>
2022-08-25 15:32:45 +0000

    changed output to full sentences.

--- hello.c
+++ hello.c
@@ -11,8 +11,8 @@
 int main(int argc, char **argv) {
 
   printf("Hello World!\n");
-  printf("Matt Carroll\n");
-  printf("Electrical Engineering\n");
+  printf("My name is Matt Carroll.\n");
+  printf("My major is Electrical Engineering\n");
 
   return 0;
 }
